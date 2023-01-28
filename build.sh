46c4486736f241b0d851da17dadcaa437122b0a1
horizon86 <593722012@qq.com>
2023-01-28 18:43:31 +0800

    Bug fix.

--- build.sh
+++ build.sh
@@ -9,8 +9,7 @@ if [ -n "`echo ${my_number} | sed -n '/^[0-9][0-9]*$/p'`" ];then
     if [ $my_number -eq $buildNumber ];then
         exit 0
     fi
-else
-    curl -O https://hub.spigotmc.org/jenkins/job/BuildTools/lastSuccessfulBuild/artifact/target/BuildTools.jar
-    java -jar -Xmx1024M BuildTools.jar
-    echo -n $buildNumber > new_build.txt
 fi
+curl -O https://hub.spigotmc.org/jenkins/job/BuildTools/lastSuccessfulBuild/artifact/target/BuildTools.jar
+java -jar -Xmx1024M BuildTools.jar
+echo -n $buildNumber > new_build.txt

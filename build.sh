cdb5d19b80b2e8f90bfb7b6f1ff2db4fe1830f5d
horizon86 <49552732+horizon86@users.noreply.github.com>
2023-03-08 17:37:44 +0800

    修复网络错误导致构建重复的bug

--- build.sh
+++ build.sh
@@ -4,12 +4,19 @@ set -euo pipefail
 buildNumber=`curl https://hub.spigotmc.org/jenkins/job/BuildTools/lastSuccessfulBuild/buildNumber`
 my_number=`curl https://api.github.com/repos/horizon86/spigotAutoBuild/releases/latest | jq -r '.tag_name'`
 
-if [ -n "`echo ${my_number} | sed -n '/^[0-9][0-9]*$/p'`" ];then
+echo latest spigot build tools release Number: $buildNumber
+echo latest my_build number: $my_number
+
+# Some times because of network error, it may not be digital, then we exit.
+if [ -n "`echo ${my_number} | sed -n '/^[0-9][0-9]*$/p'`" -a -n "`echo ${buildNumber} | sed -n '/^[0-9][0-9]*$/p'`" ];then
 
     if [ $my_number -eq $buildNumber ];then
         exit 0
     fi
+else
+    exit 0
 fi
+
 curl -O https://hub.spigotmc.org/jenkins/job/BuildTools/lastSuccessfulBuild/artifact/target/BuildTools.jar
 java -jar -Xmx1024M BuildTools.jar
 echo -n $buildNumber > new_build.txt

ce846842ce43db618752e002a8be29a97ae872c1
horizon86 <49552732+horizon86@users.noreply.github.com>
2024-02-18 20:39:06 +0800

    Update upload.sh

--- upload.sh
+++ upload.sh
@@ -2,5 +2,5 @@ if [ -f 'new_build.txt' ];then
     TAGNAME=$(cat new_build.txt)
     NAME="$(for a in spigot-*.jar; do echo $a; done)"
     # hub release create $(for a in spigot-*.jar; do echo -a $a; done) -m "$NAME" -t "main" "$TAGNAME"
-    gh release create "$TAGNAME" --target "main" --title "$NAME" $(for a in spigot-*.jar; do echo -a $a; done)
+    gh release create "$TAGNAME" --target "main" --title "$NAME" spigot-*.jar
 fi
